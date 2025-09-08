98d421bdce66c01a144f20eba752db4e0ee5b928
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-08 15:43:32 +0500

    Update Task10Main.java

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -3,15 +3,17 @@ package com.example.task10;
 public class Task10Main {
     public static void main(String[] args) {
         int[] arr = {7, 5, 9};
-        System.out.println(numMin(arr));
+        System.out.println(numMin(arr))
+        System.out.println(numMin(new int[]{5, 3, 3, 1, 1, 2}));
+        System.out.println(numMin(new int[]{10, 20, 10, 5, 5}));
     }
 
     static int numMin(int[] arr) {
-        int max = 10000000;
+        int min = arr[0];
         var index = 0;
         for(int i = 0; i < arr.length; i++)
         {
-            if(arr[i] < max)
+            if(arr[i] <= max)
             {
                 index = i;
                 max = arr[i];
